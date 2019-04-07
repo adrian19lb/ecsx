5dae0aee92df4101ae7525d0de5c46c97310becc
adrian19 <adrian19lb@gmail.com>
2019-04-07 13:55:41 +0200

    Add constructor to VelocityComponent struct

--- include/ecsx/Component/VelocityComponent.hpp
+++ include/ecsx/Component/VelocityComponent.hpp
@@ -5,7 +5,13 @@
 
 namespace ecsx::component {
 
-     struct VelocityComponent : public Component<VelocityComponent> {
+    struct VelocityComponent : public Component<VelocityComponent> {
+    public:
+        VelocityComponent(float x, float y) {
+                this->x = x;
+                this->y = y;
+            }
+         
         float x = 100.f;
         float y = 100.f;
      };
